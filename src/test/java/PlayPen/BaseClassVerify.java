0dbb4c7c400b33b5015be861773f67aee9bed20c
softwaretestlab <softwaretestlab@hotmail.com>
2021-09-26 11:03:58 +1000

    shivnewcommit

--- src/test/java/PlayPen/BaseClassVerify.java
+++ src/test/java/PlayPen/BaseClassVerify.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 import java.io.IOException;
 
 public class BaseClassVerify {
-
+//test
     @Test
     public void test01() throws IOException {
         OpenBrowser.NavToWebsite("urlCreate");
